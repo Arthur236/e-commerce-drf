dc7d565130cdbf5ea04364bc910e4b194bd21c8d
Arthur236 <arthur.thungu@gmail.com>
2018-01-25 10:16:49 +0300

    [Chore #154594590] Set up travis and coverage.

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'ecommerce.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.getenv('DATABASE_NAME'),
+        'USER': os.getenv('DATABASE_USER'),
+        'PASSWORD': os.getenv('DATABASE_PASSWORD'),
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
